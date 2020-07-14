08cadfb29084fdc059df5e22465ee31f30ee0cac
JCodePeace <mongolzzz21@gmail.com>
2020-07-14 16:07:19 +0300

    Fixed json errors and added the addNewQualification method

--- Controller/QualificationController.php
+++ Controller/QualificationController.php
@@ -18,7 +18,7 @@ class QualificationController
 
     public static function getQualifications() {
         $dao = new QualificationDao();
-        echo $dao -> getAll();
+        echo json_encode($dao -> getAll());
     }
 
     public static function getQualification($id) {
@@ -26,17 +26,30 @@ class QualificationController
         try {
             echo $dao->get($id)->toJson();
         } catch (NotFoundItemException $e) {
+            echo $e;
         }
     }
 
     public static function updateQualification($id, $abbreviation, $degree, $qualificationUA, $qualificationEN, $fieldOfStudyUA, $fieldOfStudyEN, $firstSpecialtyUA, $firstSpecialtyEN, $educationalProgramUA, $educationalProgramEN, $secondSpecialtyUA, $secondSpecialtyEN, $specializationUA, $specializationEN)
     {
-        $dao = new QualificationDao();
         try {
+            $dao = new QualificationDao();
             $previousInstance = $dao->get($id);
             $qualification = new Qualification($id, $qualificationEN, $qualificationUA, $previousInstance -> getMainFieldStudyEN(), $previousInstance -> getMainFieldStudyEN(), $degree, $previousInstance -> getDate(), $previousInstance -> getUserId(), $abbreviation, $fieldOfStudyUA, $fieldOfStudyEN, $firstSpecialtyUA, $firstSpecialtyEN, $secondSpecialtyUA, $secondSpecialtyEN, $specializationUA, $specializationEN, $educationalProgramUA, $educationalProgramEN);
             $dao -> update($qualification);
         } catch (NotFoundItemException $e) {
+        } catch (IncorrectObjectTypeException $e) {
+        }
+    }
+
+    public static function addNewQualification() {
+        try {
+            $dao = new QualificationDao();
+            /**
+             * In this case, we don't need to create any other entries because there's the trigger for creating all necessary entries after insert
+            */
+            $entity = new Qualification(-1, '', '', '', '','', date("d.m.Y"), $_SESSION['user_id'], '', '', '', '', '', '', '', '', '', '', '');
+            $dao->save($entity);
         } catch (IncorrectObjectTypeException $e) {
         }
     }
